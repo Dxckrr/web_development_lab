144a5a0b7e169377ead2131723bc401bbeffd550
Dxckrr <juandavidp1127@gmail.com>
2025-03-08 10:26:55 -0500

    fix some details on app

--- parcial/backend/src/app.ts
+++ parcial/backend/src/app.ts
@@ -1,5 +1,3 @@
 import ServerFactory from "./express/infrastructure/factory/ServerFactory";
-import MovieRouterFactory from "./movies/infrastructure/factory/MovieRouterFactory";
-const movieFactoryRouter= MovieRouterFactory.create()
-const server = ServerFactory.createServer([movieFactoryRouter])
+const server = ServerFactory.createServer([])
 server.start()
